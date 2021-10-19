8ce7a7c9d2933a38233559b6d47765dbe823798e
renan lima <renangenito@hotmail.com>
2021-10-19 14:35:54 -0300

    Adicionando data de criação e de entrega do projeto

--- infraestrutura/tabelas.js
+++ infraestrutura/tabelas.js
@@ -5,7 +5,7 @@ class Tabelas {
     }
 
     criarAtendimenstos() {
-        const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar (20), servico varchar(20) NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))'
+        const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar (20), servico varchar(20) NOT NULL, data datetime NOT NULL, dataCriacao datetime NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))'
 
         this.conexao.query(sql, erro => {
             if (erro) {
